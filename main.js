f88ab12053b0f2874f09058da1dd494506f3971b
Kounain Aftab <ishadoll03082@gmail.com>
2025-02-05 03:41:11 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var num1 = 4;
+var num2 = 2;
+console.log(num1 - num2);
+//DataTypes
+//string (Alphabets)
+//number
+//boolean (true/false , Yes/No)
+//camelCase
+//primitive DataTypes
+//String(main)
+//Symbol(&,#,$,@)
+//Boolean(main)(true/false)
+//Bignit(it is used to store big integer values that are 
+//too big to be represented by a normal js number)
+//Number(main(123456......)
+//Null(nothing/empty)
+//undefine(its not define)
---
+++ main.ts
@@ -0,0 +1,17 @@
+let a=2
+let b=2
+console.log(a/b);
+//DataTypes
+//string (Alphabets)
+//number
+//boolean (true/false , Yes/No)
+//camelCase
+//primitive DataTypes
+//String(main)
+//Symbol(&,#,$,@)
+//Boolean(main)(true/false)
+//Bignit(it is used to store big integer values that are 
+//too big to be represented by a normal js number)
+//Number(main(123456......)
+//Null(nothing/empty)
+//undefine(its not define)
